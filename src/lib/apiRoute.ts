9f753082fce731024749917991c3bca9083fff69
Ollie Etherington <ollie@etherington.xyz>
2025-08-20 10:46:10 +0100

    Cache fetched API results

--- src/lib/apiRoute.ts
+++ src/lib/apiRoute.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import type { ZodType } from "zod/v4";
 import { AdaptivePoller } from "./AdaptivePoller";
+import { LRUCache } from "lru-cache";
 
 type ContainsSlash<S extends string> = S extends `${string}/${string}`
   ? true
@@ -54,26 +55,48 @@ type UseApiRouteProps<
   skip?: boolean;
 };
 
+type FetchedResult = {
+  status: number;
+  json: unknown;
+};
+
 export class ApiRoute<
   Endpoint extends string,
   Path extends AbsolutePath<Endpoint>,
   Params extends BracketParams<Path>,
   ResponseBody,
 > {
+  private cache = new LRUCache<string, Promise<FetchedResult>>({ max: 200 });
+
   constructor(private readonly options: ApiRouteOptions<Endpoint, ResponseBody>) {}
 
-  fetch(params: Record<Params, string>) {
+  async fetch(params: Record<Params, string>): Promise<FetchedResult> {
     let endpoint: string = this.options.endpoint;
     for (const param in params) {
       const value = params[param];
       endpoint = endpoint.replaceAll(`[${param}]`, value);
     }
-    return fetch(endpoint, {
+    const result = await fetch(endpoint, {
       method: this.options.method,
       headers: {
         "Content-Type": "application/json",
       },
     });
+    return { status: result.status, json: await result.json() };
+  }
+
+  fetchWithCache(
+    params: Record<Params, string>,
+    stringifiedParams?: string,
+  ): Promise<FetchedResult> {
+    stringifiedParams ??= JSON.stringify(params);
+    const cachedValue = this.cache.get(stringifiedParams);
+    if (cachedValue) {
+      return cachedValue;
+    }
+    const value = this.fetch(params);
+    this.cache.set(stringifiedParams, value);
+    return value;
   }
 
   use({
@@ -88,8 +111,9 @@ export class ApiRoute<
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [error, setError] = useState<Error | null>(null);
 
+    const stringifiedParams = JSON.stringify(params);
     // eslint-disable-next-line react-hooks/rules-of-hooks, react-hooks/exhaustive-deps
-    const memoisedParams = useMemo(() => params, [JSON.stringify(params)]);
+    const memoisedParams = useMemo(() => params, [stringifiedParams]);
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const refetch = useCallback(async () => {
@@ -98,18 +122,24 @@ export class ApiRoute<
       }
       setLoading(true);
       try {
-        const response = await this.fetch(memoisedParams);
-
-        const rawResult = await response.json();
-        if (response.status < 200 || response.status > 299) {
-          throw new Error(
-            typeof rawResult === "string"
-              ? rawResult
-              : rawResult.error || rawResult.message || rawResult.reason,
-          );
+        const { status, json } = await this.fetchWithCache(
+          memoisedParams,
+          stringifiedParams,
+        );
+        if (status < 200 || status > 299) {
+          if (typeof json === "string") {
+            throw new Error(json);
+          } else if (!json || typeof json !== "object") {
+            throw new Error("Empty fetch response");
+          } else {
+            const error = json as Record<string, string>;
+            throw new Error(
+              error.error || error.message || error.reason || "Fetch error",
+            );
+          }
         }
 
-        const result = this.options.responseSchema.parse(rawResult);
+        const result = this.options.responseSchema.parse(json);
         setData(result);
         return true;
       } catch (e) {
@@ -120,7 +150,7 @@ export class ApiRoute<
       } finally {
         setLoading(false);
       }
-    }, [memoisedParams, skip]);
+    }, [stringifiedParams, memoisedParams, skip]);
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
