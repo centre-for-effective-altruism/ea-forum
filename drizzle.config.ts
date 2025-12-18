8148533072e1e57979fc631dcbf1816b2dbbe26b
Ollie Etherington <ollie@etherington.xyz>
2025-12-18 12:55:07 +0000

    Import drizzle table schemas

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,8 +2,8 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./src/drizzle",
-  schema: "./src/lib/db/schema.ts",
+  out: "./drizzle",
+  schema: "./drizzle/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
