906f3cf8303db5144cfbe8a903f66b94223e292c
Ollie Etherington <ollie@etherington.xyz>
2025-12-19 23:12:56 +0000

    Add missing env var types

--- ProcessEnv.d.ts
+++ ProcessEnv.d.ts
@@ -2,9 +2,8 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     // General settings
     NEXT_PUBLIC_SITE_URL: string;
-    DATABASE_URL: string;
     NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME: string;
-    NEXT_PUBLIC_SEARCH_INDEX_PREFIX?: string;
+    ENVIRONMENT: "dev" | "staging" | "prod";
 
     // Auth0 settings
     NEXT_PUBLIC_AUTH0_DOMAIN: string;
@@ -14,10 +13,19 @@ declare namespace NodeJS {
     AUTH0_ORIGINAL_DOMAIN: string;
     AUTH0_SCOPE: string;
 
-    // Elasticsearch settings
+    // Search
+    NEXT_PUBLIC_SEARCH_INDEX_PREFIX?: string;
     ELASTIC_CLOUD_ID: string;
     ELASTIC_USERNAME: string;
     ELASTIC_PASSWORD: string;
     ELASTIC_ORIGIN_DATE: string;
+
+    // Databases
+    DATABASE_URL: string;
+
+    // Site config
+    LOG_DRIZZLE_QUERIES?: "true";
+    COMMUNITY_TAG_ID: string;
+    OPPORTUNITIES_TAG_ID: string;
   }
 }
