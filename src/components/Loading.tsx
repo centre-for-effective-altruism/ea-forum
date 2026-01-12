35ac05003f90c67683d75321a275966e106a183e
Ollie Etherington <ollie@etherington.xyz>
2026-01-12 22:31:40 +0000

    Implement loading spinner

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,24 @@
-export default function Loading() {
-  // TODO
-  return "Loading";
+import clsx from "clsx";
+import { FC } from "react";
+
+const Dot: FC<{ className?: string }> = ({ className }) => (
+  <span
+    className={clsx(
+      "inline-block h-[10px] w-[10px] rounded-full animate-bounce-delay",
+      "bg-gray-600",
+      className,
+    )}
+  />
+);
+
+export default function Loading({ className }: { className?: string }) {
+  return (
+    <div
+      className={clsx("mx-auto block h-[26px] max-w-[100px] text-center", className)}
+    >
+      <Dot className="[animation-delay:-0.32s] mr-[5px]" />
+      <Dot className="[animation-delay:-0.16s] mr-[5px]" />
+      <Dot />
+    </div>
+  );
 }
