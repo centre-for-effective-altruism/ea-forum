9dae55b6e210330f9078a65114e4223bae94f2ce
Ollie Etherington <ollie@etherington.xyz>
2026-01-18 01:15:32 +0000

    Posts tooltip style improvements

--- src/components/PostsTooltip.tsx
+++ src/components/PostsTooltip.tsx
@@ -36,23 +36,43 @@ export default function PostsTooltip({
     <Tooltip
       placement={placement}
       As={As}
-      tooltipClassName="bg-white! text-black! p-0! shadow-md w-[330px]"
+      tooltipClassName="
+        bg-gray-0! text-gray-900! p-0! shadow-md w-[360px] max-w-full
+      "
       title={
         <div data-component="PostsTooltip">
-          <div className="p-2">
-            <Type style="postTitle">{title}</Type>
-            <TruncationContainer
-              items={tags.map((tag) => (
-                <TagChip tag={tag} key={tag._id} />
-              ))}
-              gap={4}
-              className="flex flex-wrap items-center gap-1 w-full overflow-hidden"
-            />
-            {htmlHighlight && <PostBody html={htmlHighlight} />}
+          <div className="px-4 py-3 flex flex-col gap-4">
+            <Type style="postTitle" className="font-[700]">
+              {title}
+            </Type>
+            {tags.length > 0 && (
+              <TruncationContainer
+                items={tags.map((tag) => (
+                  <TagChip tag={tag} key={tag._id} />
+                ))}
+                gap={4}
+                className="flex flex-wrap items-center gap-1 w-full overflow-hidden"
+              />
+            )}
+            {htmlHighlight && (
+              <PostBody
+                html={htmlHighlight}
+                className="
+                  [&_p]:text-[14px]! [&_li]:text-[14px]!
+                  [&_h1]:text-[14px]! [&_h1]:font-[700]!
+                  [&_h2]:text-[14px]! [&_h2]:font-[700]!
+                  [&_h3]:text-[14px]! [&_h3]:font-[700]!
+                  [&_h4]:text-[14px]! [&_h4]:font-[700]!
+                  [&_h5]:text-[14px]! [&_h5]:font-[700]!
+                  [&_h6]:text-[14px]! [&_h6]:font-[700]!
+                  [&_iframe]:hidden! [&_img]:hidden! [&_video]:hidden!
+                "
+              />
+            )}
           </div>
           {imageUrl && (
-            <div className="relative w-[330px] h-[120px] min-h-[60px]">
-              <Image src={imageUrl} alt={title} fill />
+            <div className="relative w-full h-[120px] min-h-[60px]">
+              <Image src={imageUrl} alt={title} fill className="object-cover" />
             </div>
           )}
         </div>
