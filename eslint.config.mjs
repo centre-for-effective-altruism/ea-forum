7b8e37c0203d845d4ef5ee4a6405446d03c97f07
Ollie Etherington <ollie@etherington.xyz>
2026-01-23 15:17:31 +0000

    Add prettier rules to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { globalIgnores } from "eslint/config";
 
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier/flat";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +12,7 @@ const __dirname = dirname(__filename);
 const eslintConfig = [
   ...nextCoreWebVitals,
   ...nextTypescript,
+  prettier,
   globalIgnores(["**/*.queries.ts"]),
   {
     rules: {
