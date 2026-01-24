87c06fb74996c1b1d0cdefa5725879e68a3fe374
Ollie Etherington <ollie@etherington.xyz>
2026-01-24 18:49:21 +0000

    Store post audio status in cookie

--- src/components/PostsPage/usePostDisplay.tsx
+++ src/components/PostsPage/usePostDisplay.tsx
@@ -9,6 +9,8 @@ import {
   useState,
 } from "react";
 import type { PostDisplay } from "@/lib/posts/postQueries";
+import { useCookiesWithConsent } from "@/lib/cookies/useCookiesWithConsent";
+import { useTracking } from "@/lib/analyticsEvents";
 
 type PostDisplayContext = {
   post: PostDisplay;
@@ -18,14 +20,23 @@ type PostDisplayContext = {
 
 const postDisplayContext = createContext<PostDisplayContext | null>(null);
 
+const audioCookie = "show_post_podcast_player";
+
 export const PostDisplayProvider: FC<{
   post: PostDisplay;
   children: ReactNode;
 }> = ({ post, children }) => {
-  const [showAudio, setShowAudio] = useState(false);
+  const { captureEvent } = useTracking();
+  const [cookies, setCookie] = useCookiesWithConsent([audioCookie]);
+  const [showAudio, setShowAudio] = useState(cookies[audioCookie] === "true");
   const toggleShowAudio = useCallback(() => {
-    setShowAudio((value) => !value);
-  }, []);
+    setShowAudio((value) => {
+      const newValue = !value;
+      setCookie(audioCookie, String(newValue), { path: "/" });
+      captureEvent("toggleAudioPlayer", { action: newValue ? "open" : "close" });
+      return newValue;
+    });
+  }, [setCookie, captureEvent]);
   return (
     <postDisplayContext.Provider value={{ post, showAudio, toggleShowAudio }}>
       {children}
