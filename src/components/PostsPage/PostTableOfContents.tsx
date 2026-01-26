a8e771bf31f9b170d75a29c55d2508f10fed250f
Ollie Etherington <ollie@etherington.xyz>
2026-01-26 15:08:19 +0000

    Add analytics context for table of contents

--- src/components/PostsPage/PostTableOfContents.tsx
+++ src/components/PostsPage/PostTableOfContents.tsx
@@ -1,5 +1,6 @@
 import type { CSSProperties } from "react";
 import type { TableOfContents } from "@/lib/revisions/htmlToTableOfContents";
+import { AnalyticsContext } from "@/lib/analyticsEvents";
 import clsx from "clsx";
 import Link from "../Link";
 import Type from "../Type";
@@ -19,39 +20,47 @@ export default function PostTableOfContents({
     return null;
   }
   return (
-    <aside
-      data-component="PostTableOfContents"
-      className={clsx("w-[260px]", className)}
+    <AnalyticsContext
+      pageSectionContext="tableOfContents"
+      componentName="PostTableOfContents"
     >
-      <nav aria-label="Table of Contents" className="flex flex-col gap-2">
-        <Type>
-          <Link href="#top" className="text-gray-1000">
-            {title}
-          </Link>
-        </Type>
-        <hr className="border-gray-300" />
-        {contents.sections.map(({ title, anchor, level }) => (
-          <Type
-            key={anchor}
-            cssStyle={{ "--anchor-level": String(level - 1) } as CSSProperties}
-            className="pl-[calc(var(--spacing)*var(--anchor-level))]"
-          >
-            <Link href={`#${anchor}`} className="text-gray-600 hover:text-gray-900">
+      <aside
+        data-component="PostTableOfContents"
+        className={clsx("w-[260px]", className)}
+      >
+        <nav aria-label="Table of Contents" className="flex flex-col gap-2">
+          <Type>
+            <Link href="#top" className="text-gray-1000">
               {title}
             </Link>
           </Type>
-        ))}
-        {commentCount && (
-          <>
-            <hr className="border-gray-300" />
-            <Type>
-              <Link href="#comments" className="text-gray-600 hover:text-gray-900">
-                {commentCount} comment{commentCount === 1 ? "" : "s"}
+          <hr className="border-gray-300" />
+          {contents.sections.map(({ title, anchor, level }) => (
+            <Type
+              key={anchor}
+              cssStyle={{ "--anchor-level": String(level - 1) } as CSSProperties}
+              className="pl-[calc(var(--spacing)*var(--anchor-level))]"
+            >
+              <Link
+                href={`#${anchor}`}
+                className="text-gray-600 hover:text-gray-900"
+              >
+                {title}
               </Link>
             </Type>
-          </>
-        )}
-      </nav>
-    </aside>
+          ))}
+          {commentCount && (
+            <>
+              <hr className="border-gray-300" />
+              <Type>
+                <Link href="#comments" className="text-gray-600 hover:text-gray-900">
+                  {commentCount} comment{commentCount === 1 ? "" : "s"}
+                </Link>
+              </Type>
+            </>
+          )}
+        </nav>
+      </aside>
+    </AnalyticsContext>
   );
 }
