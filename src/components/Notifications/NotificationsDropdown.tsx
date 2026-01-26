882f25f7d968a1dedd5957583730e5f4ae83e837
Ollie Etherington <ollie@etherington.xyz>
2026-01-26 23:20:08 +0000

    Load more notifications

--- src/components/Notifications/NotificationsDropdown.tsx
+++ src/components/Notifications/NotificationsDropdown.tsx
@@ -1,16 +1,84 @@
-import type { ReactNode } from "react";
+import { useEffect, useState, ReactNode, useCallback, useMemo } from "react";
 import { AnalyticsContext } from "@/lib/analyticsEvents";
-import { useNotifications } from "./NotificationsProvider";
+import { fetchNotificationsAction } from "@/lib/notifications/notificationActions";
+import type { NotificationDisplay as TNotificationDisplay } from "@/lib/notifications/notificationDisplayTypes";
+import range from "lodash/range";
+import debounce from "lodash/debounce";
+import toast from "react-hot-toast";
+import NotificationDisplaySkeleton from "./NotificationDisplaySkeleton";
+import InfiniteLoadTrigger from "../InfiniteLoadTrigger";
 import NotificationDisplay from "./NotificationDisplay";
 import Dropdown from "../Dropdown/Dropdown";
 import Type from "../Type";
+
+const PAGE_SIZE = 20;
 
 export default function NotificationsDropdown({
   children,
 }: Readonly<{
   children: ReactNode;
 }>) {
-  const { notifications } = useNotifications();
+  const [pages, setPages] = useState<(TNotificationDisplay[] | null)[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+
+  const loadPage = useCallback(async (pageIndex: number) => {
+    try {
+      setPages((prev) => {
+        if (prev.length >= pageIndex) {
+          return prev;
+        }
+        const next = [...prev];
+        while (next.length <= pageIndex) {
+          next.push(null);
+        }
+        return next;
+      });
+
+      const { data = [] } = await fetchNotificationsAction({
+        offset: pageIndex * PAGE_SIZE,
+        limit: PAGE_SIZE,
+      });
+
+      setPages((prev) => {
+        const next = [...prev];
+        while (next.length <= pageIndex) {
+          next.push(null);
+        }
+        if (next[pageIndex] === null) {
+          next[pageIndex] = data;
+        }
+        return next;
+      });
+
+      if (data.length < PAGE_SIZE) {
+        setHasMore(false);
+      }
+    } catch (e) {
+      toast.error("Failed to load notifications");
+      console.error(e);
+    }
+  }, []);
+
+  useEffect(() => {
+    void loadPage(0);
+  }, [loadPage]);
+
+  const loadMore = useMemo(
+    () =>
+      debounce(() => {
+        if (!hasMore) {
+          return;
+        }
+        const nextPage = pages.length;
+        void loadPage(nextPage);
+      }, 1000),
+    [hasMore, pages.length, loadPage],
+  );
+
+  useEffect(() => () => loadMore.cancel(), [loadMore]);
+
+  const isAnyLoading = pages.some((item) => !item);
+
   return (
     <AnalyticsContext pageSectionContext="notificationsPopover">
       <Dropdown
@@ -19,7 +87,7 @@ export default function NotificationsDropdown({
           <div
             data-component="NotificationsDropdown"
             className={`
-              bg-white rounded shadow px-1 py-2 border border-gray-100
+              bg-gray-0 rounded shadow px-1 py-2 border border-gray-100
               w-[400px] max-w-full max-h-[90vh] overflow-y-auto
             `}
           >
@@ -32,12 +100,31 @@ export default function NotificationsDropdown({
                 Posts & comments
               </Type>
             </div>
-            {notifications.map((notification) => (
-              <NotificationDisplay
-                key={notification._id}
-                notification={notification}
-              />
-            ))}
+            {pages.map((page, pageIndex) =>
+              page
+                ? page.map((notification) => (
+                    <NotificationDisplay
+                      key={notification._id}
+                      notification={notification}
+                    />
+                  ))
+                : range(PAGE_SIZE).map((i) => (
+                    <NotificationDisplaySkeleton key={`${pageIndex}${i}`} />
+                  )),
+            )}
+            {hasMore && !isAnyLoading && (
+              <>
+                <InfiniteLoadTrigger onTrigger={loadMore} />
+                {range(PAGE_SIZE).map((i) => (
+                  <NotificationDisplaySkeleton key={i} />
+                ))}
+              </>
+            )}
+            {!hasMore && (
+              <Type className="text-gray-600 text-center mt-8 mb-6">
+                No more notifications
+              </Type>
+            )}
           </div>
         }
       >
