0203ff0ccd82c0e37385a205697e1c860ce7af2e
Ollie Etherington <ollie@etherington.xyz>
2026-02-11 12:49:16 +0000

    Migrate report actions to use rpc

---
+++ src/lib/reports/reportsRouter.ts
@@ -0,0 +1,35 @@
+import { os } from "@orpc/server";
+import { z } from "zod/v4";
+import { getCurrentUser } from "../users/currentUser";
+import { createCommentReport, createPostReport } from "./reportMutations";
+
+export const reports = {
+  createPost: os
+    .input(
+      z.object({
+        postId: z.string(),
+        description: z.string(),
+      }),
+    )
+    .handler(async ({ input: { postId, description } }) => {
+      const currentUser = await getCurrentUser();
+      if (!currentUser) {
+        throw new Error("Please login");
+      }
+      await createPostReport(currentUser, postId, description);
+    }),
+  createComment: os
+    .input(
+      z.object({
+        commentId: z.string(),
+        description: z.string(),
+      }),
+    )
+    .handler(async ({ input: { commentId, description } }) => {
+      const currentUser = await getCurrentUser();
+      if (!currentUser) {
+        throw new Error("Please login");
+      }
+      await createCommentReport(currentUser, commentId, description);
+    }),
+};
--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,10 +1,12 @@
 import "server-only";
-import { vote } from "./votes/voteRouter";
 import { recentDiscussions } from "./recentDiscussions/recentDiscussionsRouter";
+import { reports } from "./reports/reportsRouter";
+import { vote } from "./votes/voteRouter";
 
 export const router = {
-  vote,
   recentDiscussions,
+  reports,
+  vote,
 };
 
 export type Router = typeof router;
