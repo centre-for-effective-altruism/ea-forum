af1b20f3a9d2d400eae4d9ec942b7e65f1b9ec1a
Ollie Etherington <ollie@etherington.xyz>
2026-03-24 10:43:45 +0000

    Add border radius to blocks in quick takes item skeleton

--- src/components/QuickTakes/QuickTakeItemSkeleton.tsx
+++ src/components/QuickTakes/QuickTakeItemSkeleton.tsx
@@ -9,20 +9,20 @@ export default function QuickTakeItemSkeleton() {
     >
       <div className="flex flex-row w-full gap-2 items-center mb-2">
         <div className="flex items-center justify-center gap-1 px-2">
-          <div className="h-3 w-5 bg-gray-200" />
+          <div className="h-3 w-5 bg-gray-200 rounded" />
           <SoftArrowUpIcon className="text-gray-400" />
         </div>
-        <div className="h-3 w-40 bg-gray-300" />
-        <div className="h-3 w-30 bg-gray-200" />
+        <div className="h-3 w-40 bg-gray-300 rounded" />
+        <div className="h-3 w-30 bg-gray-200 rounded" />
         <div className="grow" />
         <div className="flex flex-row gap-1">
-          <div className="h-3 w-10 bg-gray-200" />
+          <div className="h-3 w-10 bg-gray-200 rounded" />
         </div>
-        <div className="h-3 w-2 bg-gray-200" />
+        <div className="h-3 w-2 bg-gray-200 rounded" />
       </div>
       <div className="flex flex-col gap-1">
-        <div className="h-3 w-full bg-gray-200" />
-        <div className="h-3 w-full bg-gray-200" />
+        <div className="h-3 w-full bg-gray-200 rounded" />
+        <div className="h-3 w-full bg-gray-200 rounded" />
       </div>
     </article>
   );
