99f5f027af2c7a130b0e05dacdadc3ad5e3bb3d8
Ollie Etherington <ollie@etherington.xyz>
2026-04-24 10:52:53 +0100

    Add popovers to sidebar posts

--- src/components/HomePage/HomeSidebar/HomeSidebarEvent.tsx
+++ src/components/HomePage/HomeSidebar/HomeSidebarEvent.tsx
@@ -1,29 +1,32 @@
-import type { SidebarEventItem } from "@/lib/posts/postLists";
+import type { PostListItem } from "@/lib/posts/postLists";
 import { getEventLocation, postGetPageUrl } from "@/lib/posts/postsHelpers";
 import { formatShortDate } from "@/lib/timeUtils";
+import PostsTooltip from "@/components/PostsTooltip";
 import Type from "../../Type";
 import Link from "../../Link";
 
 export default function HomeSidebarEvent({
   post,
 }: Readonly<{
-  post: SidebarEventItem;
+  post: PostListItem;
 }>) {
   const { title, startTime } = post;
   const pageUrl = postGetPageUrl({ post });
   return (
-    <Link
-      href={pageUrl}
-      className="block py-1 hover:opacity-70"
-      data-component="HomeSidebarPost"
-    >
-      <Type style="bodySmall" className="font-[600] truncate">
-        {title}
-      </Type>
-      <Type style="bodySmall" className="text-gray-600">
-        <span className="mr-2">{startTime && formatShortDate(startTime)}</span>
-        {getEventLocation(post)}
-      </Type>
-    </Link>
+    <PostsTooltip post={post} placement="left-start">
+      <Link
+        href={pageUrl}
+        className="block py-1 hover:opacity-70"
+        data-component="HomeSidebarPost"
+      >
+        <Type style="bodySmall" className="font-[600] truncate">
+          {title}
+        </Type>
+        <Type style="bodySmall" className="text-gray-600">
+          {startTime && <span className="mr-2">{formatShortDate(startTime)}</span>}
+          {getEventLocation(post)}
+        </Type>
+      </Link>
+    </PostsTooltip>
   );
 }
