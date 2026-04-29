b63af70eb5c7e840c65bc5a16dce8317db9625a9
Ollie Etherington <ollie@etherington.xyz>
2026-04-29 15:54:35 +0200

    Add styles for tag mention hits

--- src/components/Search/TagMentionHit.tsx
+++ src/components/Search/TagMentionHit.tsx
@@ -1,6 +1,12 @@
 import type { SearchTag } from "@/lib/search/searchDocuments";
+import TagIcon from "@heroicons/react/24/outline/TagIcon";
+import Type from "../Type";
 
-export default function TagMentionHit({}: Readonly<{ hit: SearchTag }>) {
-  // TODO: Implement search mentions
-  return null;
+export default function TagMentionHit({ hit }: Readonly<{ hit: SearchTag }>) {
+  return (
+    <article data-component="TagMentionHit" className="flex items-center gap-2">
+      <TagIcon className="w-4" />
+      <Type style="bodySmall">{hit.name}</Type>
+    </article>
+  );
 }
