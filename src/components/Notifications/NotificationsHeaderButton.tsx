0d641412ba8d4ea544b46544c72602ea495ba5c1
Ollie Etherington <ollie@etherington.xyz>
2026-05-13 13:07:13 +0100

    Implement notification polling

--- src/components/Notifications/NotificationsHeaderButton.tsx
+++ src/components/Notifications/NotificationsHeaderButton.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { captureException } from "@sentry/nextjs";
 import type { UserKarmaChanges } from "@/lib/users/karmaChangesTypes";
 import { userHasKarmaChange } from "@/lib/users/userHelpers";
 import { useCurrentUser } from "@/lib/hooks/useCurrentUser";
+import { usePolling } from "@/lib/hooks/usePolling";
 import { rpc } from "@/lib/rpc";
 import clsx from "clsx";
 import StarIcon from "@heroicons/react/24/solid/StarIcon";
@@ -37,9 +38,10 @@ export default function NotificationsHeaderButton() {
     }
   }, [currentUser]);
 
-  useEffect(() => {
-    void refetch();
-  }, [refetch]);
+  usePolling({
+    callback: refetch,
+    intervalMs: 60000,
+  });
 
   if (!currentUser) {
     return null;
@@ -50,6 +52,9 @@ export default function NotificationsHeaderButton() {
 
   return (
     <NotificationsDropdown
+      // Add a key to force the notifications list to reload when the number of
+      // unread notifications changes
+      key={unreadNotifications}
       karmaChanges={karmaChanges}
       onOpen={refetch}
       className="relative"
