adb400f3311336736fd444591bda39f3ddfcc47e
Sarah Cheng <sarah.cheng@centreforeffectivealtruism.org>
2026-05-20 14:39:22 -0400

    Switch serif font to Source Serif

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from "react";
 import type { Metadata } from "next";
-import { Charis_SIL, Inter } from "next/font/google";
+import { Inter, Source_Serif_4 } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import { getSiteLogoUrl } from "@/lib/cloudinary/cloudinaryHelpers";
 import clsx from "clsx";
@@ -19,9 +19,11 @@ const inter = Inter({
   preload: true,
 });
 
-const charis = Charis_SIL({
-  weight: ["400", "700"],
-  variable: "--font-charis",
+const sourceSerif = Source_Serif_4({
+  weight: "variable",
+  style: ["normal", "italic"],
+  axes: ["opsz"],
+  variable: "--font-source-serif",
   display: "swap",
   preload: true,
 });
@@ -54,7 +56,7 @@ export default function RootLayout({
           "antialiased text-size-adjust-none w-full min-h-screen flex flex-col",
           "bg-background text-foreground font-sans",
           inter.variable,
-          charis.variable,
+          sourceSerif.variable,
         )}
       >
         <Providers>
