0289d8cdb0d513ad2a8529c65ff3016d9eeb2149
Ollie Etherington <ollie@etherington.xyz>
2026-06-12 15:50:26 +0100

    Collapsed pingbacks by default

--- src/components/PostsPage/PostPingbacks.tsx
+++ src/components/PostsPage/PostPingbacks.tsx
@@ -1,11 +1,8 @@
 import type { CurrentUser } from "@/lib/users/currentUser";
 import { fetchPingbackPosts } from "@/lib/posts/postLists";
-import { postGetPageUrl } from "@/lib/posts/postsHelpers";
-import PostsTooltip from "../PostsTooltip";
+import PostPingbacksList from "./PostPingbacksList";
 import Tooltip from "../Tooltip";
-import Score from "../Score";
 import Type from "../Type";
-import Link from "../Link";
 
 export default async function PostPingbacks({
   postId,
@@ -21,7 +18,7 @@ export default async function PostPingbacks({
     return null;
   }
   return (
-    <div data-component="PostPingbacks" className={className}>
+    <section data-component="PostPingbacks" className={className}>
       <Tooltip
         title={<Type style="bodySmall">Posts that linked to this post</Type>}
         placement="top-start"
@@ -31,22 +28,7 @@ export default async function PostPingbacks({
           Mentioned in
         </Type>
       </Tooltip>
-      {pingbacks.map((post) => (
-        <PostsTooltip key={post._id} post={post}>
-          <Link
-            href={postGetPageUrl({ post })}
-            className="flex items-center gap-2 hover:bg-gray-100/70 rounded py-1"
-          >
-            <Score
-              baseScore={post.baseScore}
-              voteCount={post.voteCount}
-              orientation="horizontal"
-              className="w-[50px]"
-            />
-            <Type style="bodyHeavy">{post.title}</Type>
-          </Link>
-        </PostsTooltip>
-      ))}
-    </div>
+      <PostPingbacksList pingbacks={pingbacks} />
+    </section>
   );
 }
