de8b5c9bac43179de3e1c27d6d5c40d3cbeabb3a
Ollie Etherington <ollie@etherington.xyz>
2026-06-15 21:54:16 +0100

    Implement sending analytics events from the server

--- src/lib/analytics/captureEvent.ts
+++
@@ -1,8 +0,0 @@
-import "server-only";
-import type { Json } from "../typeHelpers";
-
-export const captureServerEvent = (name: string, props: Json) => {
-  // TODO: Implement captureEvent
-  void name;
-  void props;
-};
--- src/lib/analyticsEvents.tsx
+++ src/lib/analyticsEvents.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { createContext, ReactNode, useCallback, useEffect, useMemo } from "react";
-import { useIsInView } from "./hooks/useIsInView";
 import type { Json, JsonRecord } from "./typeHelpers";
-import { useCurrentUser } from "./hooks/useCurrentUser";
-import { useClientId } from "./hooks/useClientId";
-import { RateLimiter } from "./rateLimiter";
-import throttle from "lodash/throttle";
-import { isAnyTest, isDevelopment } from "./environment";
+import { AnalyticsEvent } from "./analytics/analyticsHelpers";
 import { formatConsoleDate } from "./timeUtils";
+import { useCurrentUser } from "./hooks/useCurrentUser";
+import { useIsInView } from "./hooks/useIsInView";
+import { useClientId } from "./hooks/useClientId";
+import {
+  throttledFlushClientEvents,
+  throttledStoreEvent,
+} from "./analytics/storeEvent";
 
 type PostsViewTerms = Record<string, unknown>;
 
@@ -64,16 +66,6 @@ export type AnalyticsProps = {
 
 export type EventProps = AnalyticsProps | Record<string, Json | undefined>;
 
-const showAnalyticsDebug = () => isDevelopment && !isAnyTest();
-
-type AnalyticsEvent = {
-  type: string;
-  timestamp: Date;
-  props: JsonRecord;
-};
-
-const pendingAnalyticsEvents: AnalyticsEvent[] = [];
-
 const stringToColor = (s: string) =>
   `hsl(${[...s].reduce((a, c) => a + c.charCodeAt(0), 0) % 360} 70% 50%)`;
 
@@ -100,118 +92,6 @@ const browserConsoleLogAnalyticsEvent = (
   console.log("[[time of day]]", formatConsoleDate(new Date()));
   // eslint-disable-next-line no-console
   console.groupEnd();
-};
-
-// Analytics events have two rate limits, one denominated in events per second,
-// the other denominated in uncompressed kilobytes per second. Each of these
-// has a burst limit and a steady-state limit. If either rate limit is exceeded,
-// a rateLimitExceeded event is sent instead of the original event.
-//
-// For purposes of calculating rate limits, the size of an event is the JSON
-// string length. This undercounts slightly due to Unicode and protocol
-// overhead, and overcounts greatly due to compression.
-const burstLimitEventCount = 10;
-const burstLimitKB = 20;
-const rateLimitEventsPerSec = 3.0;
-const rateLimitKBps = 5;
-const rateLimitEventIntervalMs = 5000;
-const eventTypeLimiters: Record<
-  string,
-  {
-    eventCount: RateLimiter;
-    eventBandwidth: RateLimiter;
-    exceeded: () => void;
-  }
-> = {};
-
-const throttledStoreEvent = (event: AnalyticsEvent) => {
-  const now = new Date();
-  const eventType = event.type;
-  const eventSize = JSON.stringify(event).length;
-  if (!(eventType in eventTypeLimiters)) {
-    eventTypeLimiters[eventType] = {
-      eventCount: new RateLimiter({
-        burstLimit: burstLimitEventCount,
-        steadyStateLimit: rateLimitEventsPerSec,
-        timestamp: now,
-      }),
-      eventBandwidth: new RateLimiter({
-        burstLimit: burstLimitKB * 1024,
-        steadyStateLimit: rateLimitKBps * 1024,
-        timestamp: now,
-      }),
-      exceeded: throttle(() => {
-        pendingAnalyticsEvents.push({
-          type: "rateLimitExceeded",
-          timestamp: now,
-          props: {
-            originalType: eventType,
-          },
-        });
-      }, rateLimitEventIntervalMs),
-    };
-  }
-  const limiters = eventTypeLimiters[eventType];
-  limiters.eventCount.advanceTime(now);
-  limiters.eventBandwidth.advanceTime(now);
-
-  if (
-    limiters.eventCount.canConsumeResource(1) &&
-    limiters.eventBandwidth.canConsumeResource(eventSize)
-  ) {
-    if (showAnalyticsDebug()) {
-      browserConsoleLogAnalyticsEvent(event, false);
-    }
-    limiters.eventCount.consumeResource(1);
-    limiters.eventBandwidth.consumeResource(eventSize);
-    pendingAnalyticsEvents.push(event);
-  } else {
-    if (showAnalyticsDebug()) {
-      browserConsoleLogAnalyticsEvent(event, true);
-    }
-    limiters.exceeded();
-  }
-};
-
-/**
- * Send a request from the client to the server with an array of events.
- * Available only on the client and when the react tree is mounted.
- */
-const clientWriteEvents = async (events: AnalyticsEvent[]) => {
-  await fetch("/analyticsEvent", {
-    method: "POST",
-    body: JSON.stringify({
-      events,
-      now: new Date(),
-    }),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-};
-
-const flushClientEvents = (force: boolean = false) => {
-  if (!pendingAnalyticsEvents.length) {
-    return;
-  }
-  // Wait for tab id to be initialized
-  if (!force && !window.tabId) {
-    return;
-  }
-  const eventsToWrite = pendingAnalyticsEvents;
-  pendingAnalyticsEvents.length = 0;
-  void clientWriteEvents(eventsToWrite);
-};
-
-let lastFlushedAt: Date | null = null;
-
-const throttledFlushClientEvents = () => {
-  const flushIntervalMs = 1000;
-  const now = new Date();
-  if (!lastFlushedAt || now.getTime() - lastFlushedAt.getTime() > flushIntervalMs) {
-    lastFlushedAt = now;
-    flushClientEvents();
-  }
 };
 
 // An empty object, used as an argument default value. If the argument default
@@ -244,7 +124,7 @@ export const useTracking = ({
           ...trackingData,
         } as JsonRecord,
       };
-      throttledStoreEvent(event);
+      throttledStoreEvent(event, browserConsoleLogAnalyticsEvent);
       throttledFlushClientEvents();
     },
     [currentUser, clientId, trackingContext, eventProps, eventType],
