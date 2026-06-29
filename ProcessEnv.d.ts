39ff1f0efd8677c411ee77ac4aef09b30241c660
Ollie Etherington <ollie@etherington.xyz>
2026-06-29 13:15:32 +0100

    Enable source maps in sentry

--- ProcessEnv.d.ts
+++ ProcessEnv.d.ts
@@ -57,6 +57,7 @@ declare namespace NodeJS {
     NEXT_PUBLIC_RECAPTCHA_KEY: string;
     RECAPTCHA_PRIVATE_KEY: string;
     NEXT_PUBLIC_SENTRY_DSN: string;
+    SENTRY_AUTH_TOKEN?: string;
     INTERCOM_TOKEN: string;
     PANGRAM_API_KEY: string;
 
