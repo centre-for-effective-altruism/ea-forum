c1f76113505ba6988c05063f7d8dbdef3d0f8910
Ollie Etherington <ollie@etherington.xyz>
2026-07-27 11:47:47 +0100

    More homepage layout updates

--- src/components/HomePage/HomePageMagicTab.tsx
+++ src/components/HomePage/HomePageMagicTab.tsx
@@ -42,70 +42,70 @@ export default function HomePageMagicTab({
     return null;
   }
 
-  if (currentTag) {
-    return (
-      <div data-component="HomePageMagicTab">
-        <HomePageTagBar coreTags={coreTags} className="mb-5" />
-        <PostsList
-          key={currentTag._id}
-          posts={[]}
-          maxOffset={200}
-          loadMoreView={{
-            view: "frontpage",
-            limit: 30,
-            onlyTagId: currentTag._id,
-          }}
-        />
+  const content = currentTag ? (
+    <>
+      <HomePageTagBar coreTags={coreTags} className="mb-5" />
+      <PostsList
+        key={currentTag._id}
+        posts={[]}
+        maxOffset={200}
+        loadMoreView={{
+          view: "frontpage",
+          limit: 30,
+          onlyTagId: currentTag._id,
+        }}
+      />
+    </>
+  ) : (
+    <>
+      {spotlight && <Spotlight spotlight={spotlight} className="mt-6 mb-4" />}
+      <FilterSettingsProvider>
+        <div className="mb-5">
+          <div className="flex items-center justify-between gap-4">
+            <HomePageTagBar coreTags={coreTags} className="min-w-0" />
+            <FilterSettingsToggle />
+          </div>
+          <FilterSettingsEditor className="mt-2" />
+        </div>
+        <div className="mb-2">{stickyPostsList}</div>
+        <div className="mb-10">
+          <Suspense
+            fallback={<PostsListSkeleton count={12} viewType="fromContext" />}
+          >
+            {frontpagePostsList}
+          </Suspense>
+        </div>
+      </FilterSettingsProvider>
+      {process.env.NEXT_PUBLIC_COMMUNITY_TAG_ID && (
+        <HomePageCommunitySection className="mb-10">
+          {communityPostsList}
+        </HomePageCommunitySection>
+      )}
+      <div className="flex flex-col-reverse mobile-nav:grid grid-cols-2 gap-x-4">
+        <HomePagePopularCommentsSection className="mb-10">
+          <Suspense fallback={<QuickTakesListSkeleton count={3} />}>
+            {popularCommentsList}
+          </Suspense>
+        </HomePagePopularCommentsSection>
+        <HomePageQuickTakesSection coreTags={coreTags} className="mb-10">
+          <Suspense fallback={<QuickTakesListSkeleton count={5} />}>
+            {quickTakesList}
+          </Suspense>
+        </HomePageQuickTakesSection>
       </div>
-    );
-  }
+      <Type className="mb-2" style="sectionTitleLarge">
+        Recent discussion
+      </Type>
+      <Suspense fallback={<div className="bg-gray-200 w-full h-[400px] rounded" />}>
+        {recentDiscussions}
+      </Suspense>
+    </>
+  );
 
   return (
     <AnalyticsContext homePageTab="magic">
-      <div data-component="HomePageMagicTab">
-        {spotlight && <Spotlight spotlight={spotlight} className="mt-6 mb-4" />}
-        <FilterSettingsProvider>
-          <div className="mb-5">
-            <div className="flex items-center justify-between gap-4">
-              <HomePageTagBar coreTags={coreTags} className="min-w-0" />
-              <FilterSettingsToggle />
-            </div>
-            <FilterSettingsEditor className="mt-2" />
-          </div>
-          <div className="mb-2">{stickyPostsList}</div>
-          <div className="mb-10">
-            <Suspense
-              fallback={<PostsListSkeleton count={12} viewType="fromContext" />}
-            >
-              {frontpagePostsList}
-            </Suspense>
-          </div>
-        </FilterSettingsProvider>
-        {process.env.NEXT_PUBLIC_COMMUNITY_TAG_ID && (
-          <HomePageCommunitySection className="mb-10">
-            {communityPostsList}
-          </HomePageCommunitySection>
-        )}
-        <div className="flex flex-col-reverse mobile-nav:grid grid-cols-2 gap-x-4">
-          <HomePagePopularCommentsSection className="mb-10">
-            <Suspense fallback={<QuickTakesListSkeleton count={3} />}>
-              {popularCommentsList}
-            </Suspense>
-          </HomePagePopularCommentsSection>
-          <HomePageQuickTakesSection coreTags={coreTags} className="mb-10">
-            <Suspense fallback={<QuickTakesListSkeleton count={5} />}>
-              {quickTakesList}
-            </Suspense>
-          </HomePageQuickTakesSection>
-        </div>
-        <Type className="mb-2" style="sectionTitleLarge">
-          Recent discussion
-        </Type>
-        <Suspense
-          fallback={<div className="bg-gray-200 w-full h-[400px] rounded" />}
-        >
-          {recentDiscussions}
-        </Suspense>
+      <div data-component="HomePageMagicTab" className="max-w-[1000px]">
+        {content}
       </div>
     </AnalyticsContext>
   );
