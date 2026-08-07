4633e7eccad10476052e08889db11b2fd7c41769
Ollie Etherington <ollie@etherington.xyz>
2026-08-07 10:40:30 +0100

    Fix incorrect comment

--- src/lib/proxy/legacySiteRedirect.ts
+++ src/lib/proxy/legacySiteRedirect.ts
@@ -40,7 +40,7 @@ const newSitePatterns = [
   /^\/admin\/onboarding$/, // Admin onboarding test
   /^\/admin\/org-updates-test$/, // Admin org-updates layout test
   /^\/admin\/spotlights.*/, // Admin spotlights pages
-  /^\/admin\/swap-user-emails$/, // Admin org-updates layout test
+  /^\/admin\/swap-user-emails$/, // Admin swap user emails page
 ];
 // ...
 // Lowest precedence: Route to the *old* site if neither of the above match
